34796411ca73725a5c6f176010a0bd09d2cdd381
mhmd7233 <72330100@students.liu.edu.lb>
2026-05-30 01:37:04 +0300

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ const Footer = () => {
   return (
     <div className="footer">
       <p>Explore The World By Travel Planner © 2026</p>
+       <p>Email: travelplanner@gmail.com</p>
+
+      <p>Phone: +961 70 123 456</p>
+
+      <p>Location:Beirut, Lebanon</p>
     </div>
   );
 };
